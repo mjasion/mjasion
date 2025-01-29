7787aa217480c6c0f169aac53b9bee78a085e0f3
Marcin Jasion <5058132+mjasion@users.noreply.github.com>
2025-01-29 23:01:11 +0100

    Make upgrades to 2025 (#210)
    * Fix icons on Readme
    
    * Upgrade toha, add new background.png
    
    * Set Hugo base URL dynamically for Cloudflare Pages
    
    Added a conditional to set the base URL using the CF_PAGES_URL environment variable when deploying to Cloudflare Pages. Default behavior remains unchanged for other environments.
    
    * add icons to repository
    
    * Change readme
    
    ---------
    
    Co-authored-by: Marcin Jasion <mjasion@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -4,5 +4,10 @@ hugo mod tidy
 hugo mod npm pack
 npm install
 
-hugo --gc --minify --cleanDestinationDir
+#
+if [ -n "$CF_PAGES_URL" ]; then
+  hugo --gc --minify --cleanDestinationDir -b "$CF_PAGES_URL"
+else
+  hugo --gc --minify --cleanDestinationDir
+fi
 cp static/_redirects public/_redirects
